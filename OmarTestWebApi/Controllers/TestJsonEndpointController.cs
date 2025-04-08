ea6d0d40e84f5b8bbe55e6c723835316f2f27ba5
Omar Ebrahim <OE25306@altushq.local>
2025-04-08 10:41:15 +0100

    initial commit

---
+++ OmarTestWebApi/Controllers/TestJsonEndpointController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OmarTestWebApi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class TestJsonEndpointController
+    {
+        [Route("notify/Pension")]
+        [Route("notify/UKPayment")]
+        [Route("notify/UKPension")]
+        [Route("notify/UKPensionHolder")]
+        [Route("notify/EmployerTaxYear")]
+        [Route("notify/UKTaxCode")]
+        [HttpPost]
+        public IResult UKPost(object? e)
+        {
+            Console.WriteLine(e);
+
+            if (e == null)
+            {
+                return Results.Ok();
+            }
+
+            return Results.Json(e);
+        }
+    }
+}
